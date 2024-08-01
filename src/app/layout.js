0effef65e5f0e5e7707fa7eedf67662719974d28
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-08-01 07:09:54 -0600

    CSS

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Blog App",
-  description: "The best blog app!",
+  title: "Russ Blog App",
+  description: "Russ App Blog with NextJS",
 };
 
 export default function RootLayout({ children }) {
