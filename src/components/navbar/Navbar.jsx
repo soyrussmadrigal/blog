632cd293e710810cdfbea0e62d181fe5e2caf506
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-08-01 20:17:03 -0600

    feat: Add social media icons and navigation links to Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import styles from './navbar.module.css'
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className={styles.container}>Navbar</div>
-  )
+    <div className={styles.container}>
+      Navbar
+      <div className={styles.social}>
+        <Image src="/instagram.png" alt="Instagram" width={24} height={24} />
+        <Image src="/youtube.png" alt="YouTube" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>Russ Madrigal</div>
+      <div className={styles.links}>
+        <Link href="/">Inicio</Link>
+        <Link href="/">Acerca</Link>
+        <Link href="/">Contacto</Link>
+      </div>
+    </div>
+  );
 }
