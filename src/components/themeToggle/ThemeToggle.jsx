570ac82b03a6aa353163ace72e7e20bcc0e4a621
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-08-01 23:17:31 -0600

    chore: Update theme toggle component styling and context usage

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,21 @@
-import styles from './themeToggle.module.css';
+"use client";
+import styles from "./themeToggle.module.css";
+import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
-export default function ThemeToggle() {
+const ThemeToggle = () => {
+  const {theme} = useContext(ThemeContext);
+
+  console.log(theme)
+
   return (
-    <div className={styles.container}>ThemeToggle</div>
-  )
-}
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="moon" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
+    </div>
+  );
+};
+
+export default ThemeToggle;
